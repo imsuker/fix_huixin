09336e9cea84cbf8932813d0a86e98816bcd9a90
gukw <38353285@qq.com>
2016-03-17 19:00:02 +0800

    取url param解码方法问题

--- fix/fix.js
+++ fix/fix.js
@@ -134,7 +134,7 @@ $(function(){
         var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
         var r = window.location.search.substr(1).match(reg);
         if (r != null) {
-            return unescape(r[2]);
+            return decodeURIComponent(r[2]);
         }
         return null;
     }
